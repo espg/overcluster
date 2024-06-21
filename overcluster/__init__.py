3a95a3deda4eafeb5d35db25514f95662c4338c7
espg <refuge@rocktalus.com>
2024-06-21 13:07:38 -0400

    fork of bisectingKmeans to bisectingQmeans; version bump to 0.1.2

--- overcluster/__init__.py
+++ overcluster/__init__.py
@@ -5,3 +5,10 @@ overcluster: a library for generating overlapping cluster membership
 __version__ = "0.1.0"
 
 from .utils import *
+from ._bisect_q_means import BisectingQMeans
+
+__all__ = [
+        "BisectingQmeans",
+        "overcluster",
+        "select_central_point",
+        "slow_bisecting_kmeans"]
